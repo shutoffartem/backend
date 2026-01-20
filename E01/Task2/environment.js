f1160f52d86fe63520e84a3ced2d605122f999b5
Artem Shutov <AH4895@Labranet.jamk.fi>
2026-01-20 10:09:49 +0200

    Signed-off-by: Artem Shutov <AH4895@Labranet.jamk.fi>

---
+++ E01/Task2/environment.js
@@ -0,0 +1,5 @@
+console.log(process.version)
+console.log(process.cwd())
+console.log(process.platform)
+console.log(process.pid)
+console.log(process.env.PATH, process.env.HOME)
---
+++ E01/Task3/system-info.js
@@ -0,0 +1,10 @@
+import { os } from 'os';
+
+{
+  "type": "module",
+  "name": "exercise-1-3",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "node system-info.js",
+  }
+}
